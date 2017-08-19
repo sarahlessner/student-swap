ee30ab94eddc0ccb2a8d48755aecfe03374d9cef
Tali Saar <tali@rooster.co>
2017-08-19 15:59:29 -0700

    we can now know what users offer and ask for on signin

--- controllers/index.js
+++ controllers/index.js
@@ -2,7 +2,7 @@ module.exports = function(app) {
 	require('./landing_controller.js')(app);
 	require('./skills_match_controller.js')(app);
 	require('./user_skills_controller.js')(app);
-	require('./skills_controller.js')(app);
+	require('./add_skills_controller.js')(app);
 	require('./users_controller.js')(app);
 
 };
