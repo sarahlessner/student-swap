fc1877f85f1c8ef7d23f9c22306d1222de84041f
Sarah Lessner <sarahlessner@gmail.com>
2017-08-21 00:36:55 -0700

    enabled force sync to wipe DB real quick for testing

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ require('./controllers')(app);
 
 // Syncing our sequelize models and then starting our Express app
 // { force: true }
-db.sequelize.sync().then(function() {
+db.sequelize.sync({ force: true }).then(function() {
 
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
