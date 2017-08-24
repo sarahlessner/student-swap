782f8891f0d71d700b393137281b850b87b1cad8
Ashish Dommety <ashishuiux@gmail.com>
2017-08-23 18:07:07 -0700

    added test directory and installed mocha and chai

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -35,34 +35,5 @@ module.exports = function(app) {
           );
       }
     });
-
-// NOTE: a reminder of how complicated I was making this logic:-
-    // db.User.findAll({}).then(data => {
-    //   // console.log(data.length);
-    //   for (var i = 0; i < data.length; i++) {
-    //     // console.log(`this is the ${i} element`);
-    //     // console.log(data[i].dataValues);
-    //     // console.log("--------------------------");
-    //
-    //     if (data[i].dataValues.google_id === new_user.guid) {
-    //       console.log(`guid user ${new_user.guid} exists in the database`);
-    //       res.redirect("/homepage");
-    //     }
-    //     if (i === data.length - 1 && data[i].dataValues.google_id !==new_user.guid){
-    //       console.log("this is the last element and the user doesn't exist in the db");
-    //       db.User.create({
-    //         google_id: new_user.guid,
-    //         name: new_user.name,
-    //         email: new_user.email,
-    //         photo: new_user.picture
-    //       }).then(result => {
-    //         console.log("successfully wrote new user to database");
-    //         res.redirect("/signin");
-    //       });
-    //     }
-    //   }
-    // });
   });
-
-
 };
