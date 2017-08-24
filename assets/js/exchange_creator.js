1fae5052d3b50f473ae115e02c1c4201bbb8d8f1
Sarah Lessner <sarahlessner@gmail.com>
2017-08-23 23:06:40 -0700

    updated match controller attempting to implement promise.all in my chain of queries for perfect matches. this logic is incomplete and commented out - old working version with timer is still live. also updated exchange_creator.js to implement validation for user submission - users will receive a modal instructing them to select at least one thing that they want in exchange for their offer if they try to submit the form without checking any boxes

--- assets/js/exchange_creator.js
+++ assets/js/exchange_creator.js
@@ -1,44 +1,46 @@
 $(document).ready(function() {
-      console.log("exchange_creator loading");
+      // console.log("exchange_creator loading");
       $("#signin").on("click", function(event) {
-      event.preventDefault();
-      console.log("submitted");
-      var wanted_skills = [];
-      var userID = parseInt(localStorage.userid);
-      var offered_skill = {offer: $("#offer").val().trim(),
-      userid: userID}
+            event.preventDefault();
+            // console.log("submitted");
+            var wanted_skills = [];
+            var userID = parseInt(localStorage.userid);
+            var offered_skill = {offer: $("#offer").val().trim(),
+            userid: userID}
+            var isChecked = false;
+            for (i=0; i<18; i++) {
+                  if($("#"+i).is(':checked')) {
+                        wanted_skills.push(i);
+                        isChecked = true;
+                  }
+            }
+            if (isChecked) {
+                  var wanted_skills_object = {
+                        wantedskills: wanted_skills,
+                        userid: userID
+                  };
 
-      for (i=0; i<18; i++) {
+                  var newExchange = {
+                        wanteds: wanted_skills_object,
+                        offereds: offered_skill
+                  };
 
-         if($("#"+i).is(':checked')) {
-            wanted_skills.push(i);
-              }
-      }
+                  $.post("/newexchange", newExchange).then(function(response){
+                        console.log(response);
+                        var direction = "/" + response.direction;
+                        window.location.href = window.origin + direction;
+                  });
+            } else {
+                  bootbox.alert("you must select at least one thing you want in return for your offer");
+            }
 
-      console.log("ARRAY "+wanted_skills);
-
-
-      var wanted_skills_object = {
-            wantedskills: wanted_skills,
-            userid: userID
-
-      };
-
-      var newExchange = {
-            wanteds: wanted_skills_object,
-            offereds: offered_skill
-      };
+            // console.log("ARRAY "+wanted_skills);
 
 
-            $.post("/newexchange", newExchange).then(function(response){
-                  console.log(response);
-                  var direction = "/" + response.direction;
-                  window.location.href = window.origin + direction;
-            })
-
+           
       });
 
 
 
-
-      });
+//end document.ready
+});
