4a0b632fead17ae3fb91d9ad9dbb5969181ac1db
Sarah Lessner <sarahlessner@gmail.com>
2017-08-24 21:29:32 -0700

    added testing for user and skill models

---
+++ test/skills_model_test.js
@@ -0,0 +1,30 @@
+
+var expect = require("chai").expect;
+var Skill  = require("../models/skills.js");
+
+// console.log(Skill().skill_name);
+var testSkill = {
+	skill_name: "skill"
+}
+
+describe("skill", function() {
+  
+  it("should have a skill name", function(){
+  	expect(testSkill.skill_name).to.equal("skill");
+
+  });
+
+  it("should be a string", function(){
+  	expect(typeof testSkill.skill_name).to.equal("string");
+  });
+
+  it("should not be null", function(){
+  	expect(testSkill.skill_name).to.not.equal(null);
+  });
+
+  it("to be a type of function", function() {
+  	
+  	expect(typeof Skill).to.equal("function");
+  });
+
+});
